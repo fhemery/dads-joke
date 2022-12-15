b1f667cd89d51f88b5d0567b8b22992a1aa5b0e1
fhemery <f.hemery@hemit.fr>
2022-12-15 15:28:30 +0100

    setup : pwa + angularfire

---
+++ apps/app/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dads-joke-1eebf',
+    appId: '1:928661813818:web:03960b839e9d12ea84d303',
+    storageBucket: 'dads-joke-1eebf.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBLrbqg-0aOYK0cp6e9dBOBiC6eMIlXvbg',
+    authDomain: 'dads-joke-1eebf.firebaseapp.com',
+    messagingSenderId: '928661813818',
+  },};
--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -6,8 +6,29 @@ import {
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from './environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), importProvidersFrom(BrowserAnimationsModule)],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
+      }),
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage())
+    ),
+  ],
 }).catch((err) => console.error(err));

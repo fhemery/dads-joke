d7308a2cf3ea0ae9a1c7f93117c797ac0a36f5d4
fhemery <f.hemery@hemit.fr>
2022-12-16 09:59:35 +0100

    feat: implement routing

--- apps/app/src/app/app.routes.ts
+++ apps/app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { JokesPageComponent } from './pages/jokes-page/jokes-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: JokesPageComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '/' },
+];

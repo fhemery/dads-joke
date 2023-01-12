346e50337f92754ad2674a52ee40b9891e37c66e
fhemery <f.hemery@hemit.fr>
2023-01-12 14:27:24 +0100

    feat(profile): rxjs, with async

--- apps/app/src/app/pages/profile-page/profile-page/profile-page.component.ts
+++ apps/app/src/app/pages/profile-page/profile-page/profile-page.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileService } from './profile.service';
-import { Subject, takeUntil } from 'rxjs';
+import { Observable } from 'rxjs';
 import { UserProfile } from '../model/userProfile';
-import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,28 +11,10 @@ import { AuthService } from '../../../services/auth.service';
   templateUrl: './profile-page.component.html',
   styleUrls: ['./profile-page.component.scss'],
 })
-export class ProfilePageComponent implements OnDestroy {
-  profile: UserProfile | null = null;
-  unsubscribe$ = new Subject<void>();
+export class ProfilePageComponent {
+  profile$: Observable<UserProfile | null>;
 
-  constructor(
-    private readonly profileService: ProfileService,
-    private readonly authService: AuthService
-  ) {
-    this.authService.user$
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe((user) => {
-        if (user) {
-          const id = user?.uid;
-          this.profileService
-            .getProfile(id)
-            .pipe(takeUntil(this.unsubscribe$))
-            .subscribe((profile) => (this.profile = profile));
-        }
-      });
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.complete();
+  constructor(private readonly profileService: ProfileService) {
+    this.profile$ = this.profileService.getProfile();
   }
 }
--- apps/app/src/app/pages/profile-page/profile-page/profile.service.ts
+++ apps/app/src/app/pages/profile-page/profile-page/profile.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, switchMap } from 'rxjs';
 import { doc, docData, Firestore } from '@angular/fire/firestore';
 import { UserProfile } from '../model/userProfile';
+import { AuthService } from '../../../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
-  constructor(private readonly firestore: Firestore) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly firestore: Firestore
+  ) {}
 
-  getProfile(userId: string): Observable<UserProfile> {
+  getProfile(): Observable<UserProfile | null> {
+    return this.authService.user$.pipe(
+      switchMap((user) => {
+        if (user) {
+          const id = user?.uid;
+          return this.getProfileData(id);
+        }
+        return of(null);
+      })
+    );
+  }
+
+  private getProfileData(userId: string): Observable<UserProfile> {
     const profile = doc(this.firestore, `profiles/${userId}`);
     return docData(profile, {
       idField: 'id',

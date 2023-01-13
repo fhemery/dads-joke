9950e4178489e4df8cabb21812b9b25d0cece5c7
fhemery <f.hemery@hemit.fr>
2023-01-13 15:44:40 +0100

    feat(caching): implements jokes caching just for the example

--- apps/app/src/app/pages/jokes-page/jokes.service.ts
+++ apps/app/src/app/pages/jokes-page/jokes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Joke } from '../model/joke';
-import { map, Observable } from 'rxjs';
+import { map, Observable, ReplaySubject, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { JokePageModel } from './model/joke-page-model';
 
@@ -18,22 +18,29 @@ interface GetJokesApiResponse {
   providedIn: 'root',
 })
 export class JokesService {
+  private isInitialized = false;
+  private jokes$ = new ReplaySubject<JokePageModel>(1);
+
   constructor(private readonly httpService: HttpClient) {}
 
   getJokesWithTotal(): Observable<JokePageModel> {
-    return this.httpService
-      .get<GetJokesApiResponse>('https://icanhazdadjoke.com/search', {
-        headers: { Accept: 'application/json' },
-      })
-      .pipe(
-        map((response) => ({
-          total: response.total_jokes,
-          jokes: response.results.map((j) => ({
-            id: j.id,
-            pun: j.joke,
+    if (!this.isInitialized) {
+      this.httpService
+        .get<GetJokesApiResponse>('https://icanhazdadjoke.com/search', {
+          headers: { Accept: 'application/json' },
+        })
+        .pipe(
+          map((response) => ({
+            total: response.total_jokes,
+            jokes: response.results.map((j) => ({
+              id: j.id,
+              pun: j.joke,
+            })),
           })),
-        }))
-      );
+          tap((model) => this.jokes$.next(model))
+        );
+    }
+    return this.jokes$;
   }
 
   getJokes(): Observable<Joke[]> {

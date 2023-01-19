2d80bfd68ecc0f8d73b06cee6506f878361d00e6
fhemery <f.hemery@hemit.fr>
2023-01-19 10:44:13 +0100

    feat(perf): implements onPush

--- apps/app/src/app/pages/jokes-page/joke-detail/joke-detail.component.ts
+++ apps/app/src/app/pages/jokes-page/joke-detail/joke-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { Joke } from '../../model/joke';
@@ -9,6 +15,7 @@ import { Joke } from '../../model/joke';
   imports: [CommonModule, MatIconModule],
   templateUrl: './joke-detail.component.html',
   styleUrls: ['./joke-detail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JokeDetailComponent {
   @Input() joke: Joke | null = null;
@@ -17,5 +24,10 @@ export class JokeDetailComponent {
   setFavorite() {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     this.isFavorite.emit(this.joke!);
+  }
+
+  getPun() {
+    console.log(`Called getPun`);
+    return this.joke?.pun;
   }
 }
--- apps/app/src/app/pages/jokes-page/jokes-list/jokes-list.component.ts
+++ apps/app/src/app/pages/jokes-page/jokes-list/jokes-list.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Joke } from '../../model/joke';
 import { JokeDetailComponent } from '../joke-detail/joke-detail.component';
@@ -9,10 +15,15 @@ import { JokeDetailComponent } from '../joke-detail/joke-detail.component';
   imports: [CommonModule, JokeDetailComponent],
   templateUrl: './jokes-list.component.html',
   styleUrls: ['./jokes-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class JokesListComponent {
   @Input() jokes: Joke[] = [];
   @Output() favorite = new EventEmitter<Joke>();
+
+  trackJokeId(index: number, joke: Joke) {
+    return joke.id;
+  }
 
   setFavoriteJoke($event: Joke) {
     this.favorite.emit($event);
